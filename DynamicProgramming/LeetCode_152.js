fb0ca4a776b1bb5d2b9e53f7d61b8b13d2932bc4
NSNick <nsmitnick@gmail.com>
2024-07-29 10:46:51 +0900

    LC 152

---
+++ DynamicProgramming/LeetCode_152.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProduct = function (nums) {
+    let max = -Infinity
+    let imax = 1
+    let imin = 1
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] < 0) {
+            let temp = imax
+            imax = imin
+            imin = temp
+        }
+        imax = Math.max(imax * nums[i], nums[i])
+        imin = Math.min(imin * nums[i], nums[i])
+        max = Math.max(max, imax)
+    }
+    return max;
+};
